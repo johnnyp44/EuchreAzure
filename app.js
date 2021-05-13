51385897ff7f96c264f1992699fb24c9a937ff1e
John <john@qcrf.org>
2021-05-13 15:07:54 -0700

    basic config

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const gamesRoute = require("./routes/game");
 const appInsights = require("applicationinsights");
 const ejs = require("ejs");
 const path = require('path');
 const app = express();
 require("./config")();
-
 //Azure Portal Application Insights
 appInsights.setup('327fb52c-4445-40be-8bac-247428924e13').start();
 
@@ -17,38 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/games", gamesRoute);
+//ROUTES
+const gamesRoute = require("./routes/game");
+const indexRoute = require("./routes/index");
 
-const Game = require("./models/Game");
-const Player = require("./models/Player");
-app.get('/', async function(req, res, next) {
-  try{
-    //LEADERBOARD SECTION
-    const OGLB = await Player.aggregate([
-      {$match: {"gamesPlayed.leagueID":1 }},
-      {$group: {
-        _id: "playerID",
-        gameCount :{$sum:"gamesPlayed"}}
-      }
-    ]);
-    //console.log("Count of gamesPlayed: " + OGLB.length);
-    //for (var x=0; x<OGLB.length; x++){console.log(OGLB[x].gameCount);}
-    //GAMES SECTION
-    const OGgames = await Game.find({leagueID:1}).sort({gameDate:-1}).limit(6);
-    //console.log("OGgames has this many: " + OGgames.length);
-    const FamGames = await Game.find({leagueID:2}).sort({gameDate:-1}).limit(6);
-    //console.log("FamGames has this many: " + FamGames.length);
-    // for (var x=0; x<OGgames.length; x++){console.log(OGgames[x].gameID);}
-    //PLAYERS SECTION
-    const Players = await Player.find().sort({playerID:1});
-    //console.log("Players has this many: " + Players.length);
-    // for (var i=0; i<Players.length; i++){console.log(Players[i].firstName);}
-
-  res.render("index", {auth: "false", OGgames:OGgames, FamGames:FamGames, Players:Players});
-  }catch(err){
-    console.log(err);
-  }
-});
+app.use("/", indexRoute);
 
 const port = process.env.PORT || 3000;
 
--- controllers/game.js
+++ controllers/game.js
@@ -4,3 +4,34 @@ exports.getAllGames = async (req, res) => {
   const games = await Game.find()
   res.status(200).json(games)
 };
+
+
+// app.get('/', async function(req, res, next) {
+//   console.log("in / function");
+//   try{
+//     //LEADERBOARD SECTION
+//     const OGLB = await Player.aggregate([
+//       {$match: {"gamesPlayed.leagueID":1 }},
+//       {$group: {
+//         _id: "playerID",
+//         gameCount :{$sum:"gamesPlayed"}}
+//       }
+//     ]);
+//     //console.log("Count of gamesPlayed: " + OGLB.length);
+//     //for (var x=0; x<OGLB.length; x++){console.log(OGLB[x].gameCount);}
+//     //GAMES SECTION
+//     const OGgames = await Game.find({leagueID:1}).sort({gameDate:-1}).limit(6);
+//     //console.log("OGgames has this many: " + OGgames.length);
+//     const FamGames = await Game.find({leagueID:2}).sort({gameDate:-1}).limit(6);
+//     //console.log("FamGames has this many: " + FamGames.length);
+//     // for (var x=0; x<OGgames.length; x++){console.log(OGgames[x].gameID);}
+//     //PLAYERS SECTION
+//     const Players = await Player.find().sort({playerID:1});
+//     //console.log("Players has this many: " + Players.length);
+//     // for (var i=0; i<Players.length; i++){console.log(Players[i].firstName);}
+//
+//   res.render("index", {auth: "false", OGgames:OGgames, FamGames:FamGames, Players:Players});
+//   }catch(err){
+//     console.log(err);
+//   }
+// });
