f8aa40efa8c3d615578f2d98c127fbf10fc37393
John <john@qcrf.org>
2021-05-25 23:02:25 -0700

    db connection troubleshooting

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const { DB_USER, DB_PASSWORD, DB_USER_MONGO, DB_PASSWORD_MONGO} = process.env;
+const { COSMOS_USER, COSMOS_PASSWORD, DB_USER_MONGO, DB_PASSWORD_MONGO} = process.env;
 //url = `mongodb://localhost:27017/EuchreStats`
-url = `mongodb://${DB_USER}:${DB_PASSWORD}@euchrestats.mongo.cosmos.azure.com:10255/EuchreStats?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@euchrestats@`
+url = `mongodb://${COSMOS_USER}:${COSMOS_PASSWORD}@euchrestats.mongo.cosmos.azure.com:10255/EuchreStats?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@euchrestats@`
 //url = //`mongodb+srv://${DB_USER_MONGO}:${DB_PASSWORD_MONGO}@euchrestats.xtby3.mongodb.net/EuchreStats?retryWrites=true&w=majority`
 
 const connectDB = () => {
@@ -18,6 +18,6 @@ const connectDB = () => {
       }
     )
     .then(() => console.log(`database connected successfully`))
-    .catch((err) => console.log("db connection failed:", err.message));
+    .catch((err) => console.log("db connection failed: " + url + " with error " + err.message));
 };
 module.exports = connectDB;
